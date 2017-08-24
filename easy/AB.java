02fb8cdb5124b8b33825b7d0e386087866518c17
Reddy <anjred@amazon.com>
2017-08-23 22:17:27 -0700

    first commit

---
+++ easy/AB.java
@@ -0,0 +1,24 @@
+/**
+@author : anjred
+date : 08/23/2017
+Problem Statement
+You are given two s: N and K. Lun the dog is interested in strings that satisfy the following conditions:
+    The string has exactly N characters, each of which is either 'A' or 'B'.
+    The string s has exactly K pairs (i, j) (0 <= i < j <= N-1) such that s[i] = 'A' and s[j] = 'B'.
+If there exists a string that satisfies the conditions, find and return any such string. Otherwise, return an empty string.
+Definition
+Class: AB
+Method: createString
+Parameters: integer, integer
+Returns: string
+Method signature: String createString(int N, int K):
+**/
+
+public class AB {
+	public String createString(int N, int K){
+		StringBuilder sb = new StringBuilder("");
+
+	}
+
+
+}
